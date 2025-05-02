379637592bcd314560bbc957a7d247f8007c3c10
Ntarekp <kpntare@gmail.com>
2025-05-02 15:18:38 +0200

    Used ResponseEntity

--- SpringBoot/springboot-rest-api/src/main/java/org/kaliLearn/springboot_rest_api/controller/StudentController.java
+++ SpringBoot/springboot-rest-api/src/main/java/org/kaliLearn/springboot_rest_api/controller/StudentController.java
@@ -1,5 +1,6 @@
 package org.kaliLearn.springboot_rest_api.controller;
 
+import org.apache.coyote.Response;
 import org.kaliLearn.springboot_rest_api.bean.Student;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,22 +29,23 @@ public class StudentController {
     }
 
     @GetMapping("students")
-    public List<Student> getStudents(){
+    public ResponseEntity<List<Student>> getStudents(){
         List<Student> students = new ArrayList<>();
         students.add(new Student(1, "Tuyi","Shime"));
         students.add(new Student(2, "Kai","tare"));
         students.add(new Student(3,"John","Paul"));
-        return students;
+        return ResponseEntity.ok(students);
     }
 
     //Spring BOOT API with Path Variable
     //{id} -- means URI template variable ex:
     //http://localhost:8080/students/1/kaitare/prince
     @GetMapping("students/{id}/{first-name}/{last-name}")
-    public Student studentPathVariable(@PathVariable("id") int StudentId,
+    public ResponseEntity<Student> studentPathVariable(@PathVariable("id") int StudentId,
                                        @PathVariable("first-name") String firstName,
                                        @PathVariable("last-name") String lastName){
-        return  new Student(StudentId,lastName,firstName);
+          Student student = new Student(StudentId,lastName,firstName);
+          return ResponseEntity.ok(student);
     }
 
     // Springboot Request API with request parameter.
@@ -51,11 +53,12 @@ public class StudentController {
     // this is a querry parameter
 
     @GetMapping("students/query")
-    public Student studentRequestVariable(@RequestParam int id,
+    public ResponseEntity<Student> studentRequestVariable(@RequestParam int id,
                                           @RequestParam String firstName,
                                           @RequestParam String lastName
     ){
-        return new Student(id, firstName,lastName);
+        Student student = new Student(id, firstName,lastName);
+        return ResponseEntity.ok(student);
 
     }
 
@@ -64,29 +67,29 @@ public class StudentController {
     //@PostMapping and @RequestBody
 
     @PostMapping("students/create")
-    @ResponseStatus(HttpStatus.CREATED)
-    public Student createStudent(@RequestBody Student student){
+//    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Student> createStudent(@RequestBody Student student){
         System.out.println(student.getId());
         System.out.println(student.getFirstName());
         System.out.println(student.getLastName());
-        return student;
+        return new ResponseEntity<>(student,HttpStatus.CREATED);
     }
 
 
     //SpringBoot REST API that handles http PUT request that updates the existing
     @PutMapping("Students/{id}/update")
-    public Student updateStudent(@RequestBody Student student, @PathVariable("id") int studentId){
+    public ResponseEntity<Student> updateStudent(@RequestBody Student student, @PathVariable("id") int studentId){
         System.out.println(student.getFirstName());
         System.out.println(student.getLastName());
-        return  student;
+        return  ResponseEntity.ok(student);
     }
 
     //SpringBOOT REST API that handles the http DELETE Request - Deleting the existing resource
 
     @DeleteMapping("student/{id}/delete")
-    public String deleteStudent(@PathVariable("id") int  StudentId){
+    public ResponseEntity<String> deleteStudent(@PathVariable("id") int  StudentId){
         System.out.println(StudentId );
-        return "Student Successfully Deleted";
+        return ResponseEntity.ok("StudentDeleted Successfully");
     }
 
 }
