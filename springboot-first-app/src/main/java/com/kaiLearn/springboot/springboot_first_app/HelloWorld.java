8439bec9e553ea74e4cde9cde4b44e7528a75cf8
Ntarekp <kpntare@gmail.com>
2025-05-02 11:08:14 +0200

    Add springboot-first-app to main repo

---
+++ springboot-first-app/src/main/java/com/kaiLearn/springboot/springboot_first_app/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.kaiLearn.springboot.springboot_first_app;
+
+public class HelloWorld {
+
+    public String helloWorld(){
+        return "Hello my first real application";
+    }
+}
---
+++ springboot-first-app/src/main/java/com/kaiLearn/springboot/springboot_first_app/SpringbootFirstAppApplication.java
@@ -0,0 +1,21 @@
+package com.kaiLearn.springboot.springboot_first_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpringbootFirstAppApplication {
+
+	@Bean
+	 public HelloWorld helloWorld(){
+		 return new HelloWorld();
+	 }
+	public static void main(String[] args) {
+	ApplicationContext applicationContext = SpringApplication.run(SpringbootFirstAppApplication.class, args);
+	HelloWorld helloWorld = applicationContext.getBean(HelloWorld.class);
+		System.out.println(helloWorld.helloWorld());
+	}
+
+}
