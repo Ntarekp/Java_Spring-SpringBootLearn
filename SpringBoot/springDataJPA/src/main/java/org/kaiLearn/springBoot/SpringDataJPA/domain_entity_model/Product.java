bf9a9de92e4a70a001c3c7da4993a9933b2c0e92
Ntarekp <kpntare@gmail.com>
2025-05-28 05:05:46 +0200

    Use Lombok to reduce boilerplate code for getters and setters in Product.java

--- SpringBoot/springDataJPA/src/main/java/org/kaiLearn/springBoot/SpringDataJPA/domain_entity_model/Product.java
+++ SpringBoot/springDataJPA/src/main/java/org/kaiLearn/springBoot/SpringDataJPA/domain_entity_model/Product.java
@@ -2,11 +2,20 @@ package org.kaiLearn.springBoot.SpringDataJPA.domain_entity_model;
 
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(
         name = "tbl_products",
@@ -21,10 +30,16 @@ import java.time.LocalDateTime;
 public class Product {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+            generator = "product_generator")
+    @SequenceGenerator(
+            name = "product_generator",
+            sequenceName = "product_sequence_name",
+            allocationSize = 1
+    )
     @Column(name = "id")
     private Long id;
-    @Column(name ="stock_keeping_unit",nullable = false)
+    @Column(name = "stock_keeping_unit", nullable = false)
     private String sku;
     @Column(nullable = false)
     private String name;
@@ -32,83 +47,11 @@ public class Product {
     private BigDecimal price;
     private boolean active;
     private String imageUrl;
+    @CreationTimestamp
     private LocalDateTime dateCreated;
+
+    @UpdateTimestamp
     private LocalDateTime lastUpdated;
 
 
-    //Getters and setters
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getSku() {
-        return sku;
-    }
-
-    public void setSku(String sku) {
-        this.sku = sku;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    public boolean isActive() {
-        return active;
-    }
-
-    public void setActive(boolean active) {
-        this.active = active;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-
-    public LocalDateTime getDateCreated() {
-        return dateCreated;
-    }
-
-    public void setDateCreated(LocalDateTime dateCreated) {
-        this.dateCreated = dateCreated;
-    }
-
-    public LocalDateTime getLastUpdated() {
-        return lastUpdated;
-    }
-
-    public void setLastUpdated(LocalDateTime lastUpdated) {
-        this.lastUpdated = lastUpdated;
-    }
 }
-
