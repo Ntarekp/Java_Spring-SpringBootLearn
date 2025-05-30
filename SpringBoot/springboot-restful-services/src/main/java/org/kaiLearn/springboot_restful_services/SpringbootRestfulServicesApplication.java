b011d3d12a65ff31b08b18ba336aa7bc9c25cb83
Ntarekp <kpntare@gmail.com>
2025-05-30 05:50:55 +0200

    Project setup for springboot-restful-webservices

---
+++ SpringBoot/springboot-restful-services/src/main/java/org/kaiLearn/springboot_restful_services/SpringbootRestfulServicesApplication.java
@@ -0,0 +1,13 @@
+package org.kaiLearn.springboot_restful_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestfulServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestfulServicesApplication.class, args);
+	}
+
+}
---
+++ SpringBoot/springboot-restful-services/src/test/java/org/kaiLearn/springboot_restful_services/SpringbootRestfulServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kaiLearn.springboot_restful_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestfulServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
