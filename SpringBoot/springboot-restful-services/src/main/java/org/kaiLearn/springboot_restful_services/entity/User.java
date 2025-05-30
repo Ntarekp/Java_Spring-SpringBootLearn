f0c2696dbb8c3fd85db7bb55c5a43685fedf5432
Ntarekp <kpntare@gmail.com>
2025-05-30 06:31:07 +0200

    Configure application properties and user entity

---
+++ SpringBoot/springboot-restful-services/src/main/java/org/kaiLearn/springboot_restful_services/entity/User.java
@@ -0,0 +1,29 @@
+package org.kaiLearn.springboot_restful_services.entity;
+
+
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+
+@Entity
+@Table(name ="users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+    @Column(nullable = false)
+    private String lasName;
+    @Column(nullable = false, unique = true)
+    private String email;
+}
