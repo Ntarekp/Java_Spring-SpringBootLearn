adcb10ffb245a1ed6b1777088d4d3a651df778f1
Ntarekp <kpntare@gmail.com>
2025-05-31 21:35:31 +0200

    Update the user repository

--- SpringBoot/springboot-restful-services/src/main/java/org/kaiLearn/springboot_restful_services/service/UserService.java
+++ SpringBoot/springboot-restful-services/src/main/java/org/kaiLearn/springboot_restful_services/service/UserService.java
@@ -2,6 +2,7 @@ package org.kaiLearn.springboot_restful_services.service;
 
 import org.kaiLearn.springboot_restful_services.entity.User;
 
-public interface UserService {
-    User createUser(User user);
+public interface UserService{
+   User createUser(User user);
+
 }
--- SpringBoot/springboot-restful-services/src/main/java/org/kaiLearn/springboot_restful_services/service/impl/UserServiceImpl.java
+++ SpringBoot/springboot-restful-services/src/main/java/org/kaiLearn/springboot_restful_services/service/impl/UserServiceImpl.java
@@ -1,9 +1,11 @@
 package org.kaiLearn.springboot_restful_services.service.impl;
 
 import org.kaiLearn.springboot_restful_services.entity.User;
+import org.kaiLearn.springboot_restful_services.repository.UserRepository;
 import org.kaiLearn.springboot_restful_services.service.UserService;
 
 public class UserServiceImpl implements UserService {
+    private UserRepository userRepository
 @Override
     public User createUser(User user){
     return null;
