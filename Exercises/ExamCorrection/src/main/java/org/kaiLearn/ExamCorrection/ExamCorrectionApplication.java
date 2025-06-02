1e4ccb91df2d8182c80da803aa756087753b23ec
Ntarekp <kpntare@gmail.com>
2025-06-02 21:35:19 +0200

    Redo exam theory correction

---
+++ Exercises/ExamCorrection/src/main/java/org/kaiLearn/ExamCorrection/ExamCorrectionApplication.java
@@ -0,0 +1,13 @@
+package org.kaiLearn.ExamCorrection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamCorrectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamCorrectionApplication.class, args);
+	}
+
+}
